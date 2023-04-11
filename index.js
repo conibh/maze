5bf9d6ed5be7d03c9e6c9a86d832293be4ca98cb
Coni <coni@devs-MacBook-Pro.local>
2023-04-11 08:51:52 +0300

    started to create maze the previous demo is in another file

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-const { Engine, Render, Runner, World, Bodies } = Matter;
+const { Engine, 
+    Render, 
+    Runner, 
+    World, 
+    Bodies
+} = Matter;
+
+const width = 600;
+const height = 600;
 
 const engine = Engine.create();
 const { world } = engine;
@@ -6,20 +14,23 @@ const render  = Render.create({
     element: document.body,
     engine: engine,
     options: {
-        width: 800,
-        height: 600
+        width,
+        height,
+        wireframes: false
     }
 });
 
 Render.run(render);
 Runner.run(Runner.create(), engine);
 
+
+
 // WALLS 
 const walls = [
-    Bodies.rectangle(400, 0, 800 , 40 , { isStatic: true}),
-    Bodies.rectangle(400, 600, 800, 40, {isStatic: true}),
-    Bodies.rectangle(0, 300, 40, 600, {isStatic: true}),
-    Bodies.rectangle(800, 300, 40, 600, {isStatic: true})
+    Bodies.rectangle(width / 2, 0, width , 40 , { isStatic: true}),
+    Bodies.rectangle(width / 2, height, width, 40, {isStatic: true}),
+    Bodies.rectangle(0, height / 2, 40, width, {isStatic: true}),
+    Bodies.rectangle(width, height / 2, 40, height, {isStatic: true})
 
 ];
 World.add(world,walls);
